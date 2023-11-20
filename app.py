67dda33c676001b0689fa00ad221b46f2ca07e24
MrYious <rosariomark37@gmail.com>
2023-11-20 23:20:59 +0800

    feat: completed highlighting and reorganized result code.

--- app.py
+++ app.py
@@ -7,6 +7,65 @@ app = Flask(__name__)
 
 CORS(app)
 
+# Stop Words List
+stopwords = [
+    "isang", "tungkol", "pagkatapos", "muli", "laban",
+    "lahat", "ako", "ay", "at", "alinman", "hindi", "gaya",
+    "maging", "dahil", "naging", "bago", "pagiging", "ibaba",
+    "pagitan", "pareho", "ngunit", "pamamagitan", "maaari",
+    "ginawa", "gawin", "ginagawa", "pababa", "habang",
+    "bawat", "ilang", "para", "mula", "pa", "mayroon", "wala",
+    "may", "siya", "siya'y", "kanya", "dito", "narito", "sarili",
+    "kanyang", "paano", "ako'y", "nito", "tayo'y", "mas",
+    "karamihan", "dapat", "aking", "mismo", "ni", "palayo",
+    "beses", "lamang", "o", "iba", "atin", "ating", "mga", "labas",
+    "kaya", "kaysa", "iyon", "ang",
+    "kanilang", "kanila", "sila", "ito", "sa", "rin",
+    "hanggang", "pataas", "napakas", "tayo", "ay", "kailan",
+    "saan", "alin", "sino", "kanino", "bakit", "kasama",
+    "gusto", "ikaw", "iyo", "inyong", "ang", "na", "sa",
+    "kay", "ni", "ng", "ngayon", "ito", "ka", "sila", "ka",
+    "ngayo'y", "kapag", "kung", "saka", "siya", "siyang",
+    "sya", "sya'y", "tayo", "tulad", "yun", "yung"
+]
+
+# Hate Speech Word List
+hate_words_list = [
+  'abnormal', 'abnoy', 'animal', 'arabo', 'aso',
+  'aswang', 'baboy', 'backstabber', 'bading', 'badjao',
+  'bakla', 'balimbing', 'baliw', 'baluga', 'balyena',
+  'bansot', 'basura', 'batchoy', 'bayot', 'beho',
+  'bekimon', 'bingi', 'bingot', 'bingot', 'biot',
+  'bisakol', 'bisaya', 'bitch', 'bobita', 'bobo',
+  'bruha', 'buang', 'bulag', 'bumbay', 'bungol',
+  'burikat', 'butiki', 'buwaya', 'chekwa', 'chingchong',
+  'chink', 'corrupt', 'cunt', 'dambuhala', 'demon',
+  'demonyo', 'dugyot', 'duling', 'dumbass', 'dwende',
+  'elitista', 'engkanto', 'fag', 'faggot', 'fatass',
+  'fatty', 'gaga', 'gago', 'gasul', 'gunggong',
+  'gurang', 'hampaslupa', 'hayop', 'hipon', 'hudas',
+  'idiot', 'impakta', 'indogs', 'intsik', 'intsikbeho',
+  'inutil', 'itim', 'judas', 'kabayo', 'kalabaw',
+  'kalbo', 'kapre', 'korap', 'kulto', 'kurakot',
+  'ladyboy', 'lamang-lupa', 'laspag', 'lesbo', 'linta',
+  'mabaho', 'magnanakaw', 'maitim', 'maligno', 'manananggal',
+  'mangkukulam', 'mangmang', 'manyak', 'manyakis', 'matanda',
+  'may saltik', 'may sayad', 'mongoloid', 'multo', 'negra',
+  'negro', 'nganget', 'ngongo', 'nigga', 'nigger',
+  'nognog', 'pandak', 'pandak', 'panget', 'pango',
+  'panot', 'peenoise', 'pignoys', 'plastik', 'pokpok',
+  'prick', 'prostitute', 'pulpol', 'pussy', 'puta',
+  'retard', 'retokada', 'sadako', 'sakang', 'sakim',
+  'salot', 'satan', 'satanas', 'satanist', 'shunga',
+  'sinto-sinto', 'sinungaling', 'siraulo', 'skwater',
+  'slapsoil', 'slut', 'squammy', 'squatter', 'stupid',
+  'supot', 'taba', 'tababoy', 'tabaching-ching', 'tabachoy',
+  'tanda', 'tanga', 'tarantado', 'terrorista', 'tibo',
+  'tikbalang', 'tingting', 'tiyanak', 'tomboy', 'topak',
+  'tranny', 'trans', 'trapo', 'trash', 'traydor',
+  'tuta', 'ugly', 'ulaga', 'unano', 'unggoy'
+];
+
 # Load the TF-IDF model
 tfidf_model = joblib.load('tfidf_vectorizer.pkl')
 
@@ -35,7 +94,7 @@ def preprocessText(text):
     text = re.sub(pattern, replacement, text)
     # REMOVE: Mentions
     pattern = r'@(\w+)'
-    replacement = '@user'
+    replacement = '@USER'
     text = re.sub(pattern, replacement, text)
     # REMOVE: Whitespaces
     pattern = r'\s+'
@@ -69,27 +128,6 @@ def logistic():
 
     text = preprocessText(text)
 
-    stopwords = [
-        "isang", "tungkol", "pagkatapos", "muli", "laban",
-        "lahat", "ako", "ay", "at", "alinman", "hindi", "gaya",
-        "maging", "dahil", "naging", "bago", "pagiging", "ibaba",
-        "pagitan", "pareho", "ngunit", "pamamagitan", "maaari",
-        "ginawa", "gawin", "ginagawa", "pababa", "habang",
-        "bawat", "ilang", "para", "mula", "pa", "mayroon", "wala",
-        "may", "siya", "siya'y", "kanya", "dito", "narito", "sarili",
-        "kanyang", "paano", "ako'y", "nito", "tayo'y", "mas",
-        "karamihan", "dapat", "aking", "mismo", "ni", "palayo",
-        "beses", "lamang", "o", "iba", "atin", "ating", "mga", "labas",
-        "kaya", "kaysa", "iyon", "ang",
-        "kanilang", "kanila", "sila", "ito", "sa", "rin",
-        "hanggang", "pataas", "napakas", "tayo", "ay", "kailan",
-        "saan", "alin", "sino", "kanino", "bakit", "kasama",
-        "gusto", "ikaw", "iyo", "inyong", "ang", "na", "sa",
-        "kay", "ni", "ng", "ngayon", "ito", "ka", "sila", "ka",
-        "ngayo'y", "kapag", "kung", "saka", "siya", "siyang",
-        "sya", "sya'y", "tayo", "tulad", "yun", "yung"
-    ]
-
     words = text.split()
     filtered_words = [word for word in words if word.lower() not in stopwords]
     text = ' '.join(filtered_words)
@@ -121,30 +159,38 @@ def hybrid():
     text = data.get('text')
 
     text = preprocessText(text)
+    textArray = text.split()
 
-    hate_words_list = ["tanga", "nigga", "nigger", "bisakol", "bayot"]
     negation_words_list = [""]
 
     isRule1 = False
     isRule2 = ruleBased2(text, hate_words_list)
 
     if isRule1:
+        matching_words = [word for word in textArray if word in hate_words_list]
 
         result = {
             'model': 'rule',
-            'prediction': int(0 if isRule2 else 1),
-            'negation_detected_words': ['sample', 'sample', 'sample'],
+            'prediction': 0,
+            'negation_detected_words': matching_words,
             'rule': 1
         }
     elif isRule2:
+        matching_words = [word for word in textArray if word in hate_words_list]
 
         result = {
             'model': 'rule',
-            'prediction': int(1 if isRule2 else 0),
-            'hate_detected_words': ['sample', 'sample', 'sample'],
+            'prediction': 1,
+            'hate_detected_words': matching_words,
             'rule': 2
         }
     else:
+        text = preprocessText(text)
+
+        words = text.split()
+        filtered_words = [word for word in words if word.lower() not in stopwords]
+        text = ' '.join(filtered_words)
+
         # FEATURE EXTRACTION: Create input features via TF-IDF
         input_features = tfidf_model.transform([text])
 
