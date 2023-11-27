ba25ae68fdf29801563281b6622f0ea923550cd1
MrYious <rosariomark37@gmail.com>
2023-11-27 15:57:05 +0800

    feat: sample push

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app)
-
+# sample comment
 # Offensive Word List 151
 offensive_words_list = [
     'abnormal', 'abusado', 'adik', 'animal', 'arabo',
