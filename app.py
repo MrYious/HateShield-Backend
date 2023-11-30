e7b95295dee2543d83e37722f92aa6df5d260aae
MrYious <rosariomark37@gmail.com>
2023-11-30 10:23:28 +0800

    feat: add incomplete word correction algorithm

--- app.py
+++ app.py
@@ -164,6 +164,41 @@ def preprocessText1(text):
 
     return text
 
+def levenshtein_distance(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein_distance(s2, s1)
+
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+def wordCorrection(textArray, hatewords):
+    newTextArray = []
+
+    for word in textArray:
+        corrected_word = word
+        for hateword in hatewords:
+            # Use regex to check for case-insensitive similarity
+            if re.match(f'(?i){hateword}', word):
+                # Find the hateword with the minimum Levenshtein distance
+                corrected_word = min(hatewords, key=lambda x: levenshtein_distance(word.lower(), x.lower()))
+                break
+
+        newTextArray.append(corrected_word)
+
+    return newTextArray
+
 def ruleBased0(text, hate_words):
     text_quotations = re.findall(r'["\']([^"\']*)["\']', text)
     print(text_quotations)
